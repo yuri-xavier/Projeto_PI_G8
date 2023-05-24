37d7ef74a26aba45116154ed7af1b2cff64ccd6b
Yuri Xavier <yurix@hotmail.com.br>
2023-05-24 16:56:34 -0300

    Atualização das informações

--- javascript/salgado.js
+++ javascript/salgado.js
@@ -1,20 +1,20 @@
 // salgado ----------------------------------------------------------------------------------------------
 
-// document.addEventListener("DOMContentLoaded", function () {
-//   fetch("/assets/JSON/salgado.json")
-//     .then((res) => res.json())
-//     .then((data) => {
-//       const grid = document.querySelector(".grid");
-//       data.items.forEach((item) => {
-//         const gridItem = document.createElement("div");
-//         gridItem.classList.add("grid-item");
-//         gridItem.innerHTML = `
-//         <a href="${item.link}" target="_blank">
-//         <img src="${item.image}" alt="${item.title}" class="item-image">
-//         <h3 class="item-title">${item.title}</h3>
-//         </a>
-//         `;
-//         grid.appendChild(gridItem);
-//       });
-//     });
-// });
+document.addEventListener("DOMContentLoaded", function () {
+  fetch("/assets/JSON/salgado.json")
+    .then((res) => res.json())
+    .then((data) => {
+      const grid = document.querySelector(".grid");
+      data.items.forEach((item) => {
+        const gridItem = document.createElement("div");
+        gridItem.classList.add("grid-item");
+        gridItem.innerHTML = `
+        <a href="${item.link}" target="_blank">
+        <img src="${item.image}" alt="${item.title}" class="item-image">
+        <h3 class="item-title">${item.title}</h3>
+        </a>
+        `;
+        grid.appendChild(gridItem);
+      });
+    });
+});
