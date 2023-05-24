44054ce963ba2447dbf1afcd508baee5d9d3c1e9
Yuri Xavier <yurix@hotmail.com.br>
2023-05-23 21:16:25 -0300

    Update infos

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,3 +1,5 @@
+// MENU -----------------------------------------------------------------------------------------------------
+
 const menu = document.querySelector(".menu");
 const NavMenu = document.querySelector(".nav-menu");
 
@@ -5,3 +7,22 @@ menu.addEventListener("click", () => {
   menu.classList.toggle("ativo");
   NavMenu.classList.toggle("ativo");
 });
+
+// scroll-----------------------------------------------------------------------------------------------
+
+const scrollAnima = document.querySelector('[data-anima="scroll"]');
+const metadewindow = window.innerHeight * 2.7;
+console.log(metadewindow);
+function animaScroll() {
+  const topoItem = scrollAnima.getBoundingClientRect().top;
+
+  const ItemVisivel = topoItem - metadewindow < 0;
+
+  if (ItemVisivel) {
+    scrollAnima.classList.add("show-button");
+  } else {
+    scrollAnima.classList.remove("show-button");
+  }
+}
+
+window.addEventListener("scroll", animaScroll);
