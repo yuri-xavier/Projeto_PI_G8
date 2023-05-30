17a8ed08c0c7bbcf74772a0184167da9f772ed68
LuziC <devluziane@gmail.com>
2023-05-30 19:51:07 -0300

    form modificado, adicionado login

--- javascript/form.js
+++ javascript/form.js
@@ -1,5 +1,14 @@
+const form = document.querySelector ("#form")
+const nameInput = document.querySelector ("#name")
+const nameInput = document.querySelector ("#email")
+const nameInput = document.querySelector ("#password")
+
+
 function logar () (
 
     let login = document.getElementById ('login').value;
     let senha = document.getElementById ('senha').value;
+
+    if (login )
+
 )
